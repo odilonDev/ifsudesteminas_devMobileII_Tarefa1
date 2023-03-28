ba6c8424b3cf638e429e5a42103ceee6d54e87bb
Odilon H F Filho <odilon_tec@yahoo.com.br>
2023-03-28 15:41:53 -0300

    primeiro commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,55 @@
+import { Component } from '@angular/core';
+import { NavController, ModalController } from 'ionic-angular';
+import { AddItemPage } from '../add-item/add-item';
+import { ItemDetailPage } from '../item-detail/item-detail';
+import { Data } from '../../providers/data/data';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  public items = [];
+
+  constructor(public navCtrl: NavController, public modalCtrl: ModalController, public dataService: Data) {
+      this.dataService.getData().then((todos) =>{
+        if(todos){
+          this.items = todos;
+        }
+      });
+  }
+
+  ionViewDidLoad(){
+  }
+
+  addItem(){
+    let addModal = this.modalCtrl.create(AddItemPage);
+
+    addModal.onDidDismiss((item) =>{
+      if(item){
+        this.saveItem(item);
+      }
+    });
+
+    addModal.present();
+  }
+
+  saveItem(item){
+    this.items.push(item); 
+    this.dataService.save(this.items); 
+  }
+
+  viewItem(item){
+    this.navCtrl.push(ItemDetailPage, {
+      item: item
+    });
+  }
+
+  deleteItem(item){
+    this.items.splice(this.items.indexOf(item),1); 
+    this.dataService.save(this.items); 
+  }
+
+}
